7d04710ceab01245f59d9a005ef7b828bb4ba32b
morghull <morghull.study@gmail.com>
2021-11-08 12:00:41 +0200

    finalize lesson 55

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-"use strict";
+'use strict';
 
+const fs = require('fs/promises', 'utf-8');
+
+fs.readFile('./text.txt', { encoding: 'utf-8' }).then(
+  (file) => {
+    fs.writeFile('./newtext.txt', file + '\r\nLorem!!!', {
+      encoding: 'utf-8',
+    });
+  }
+);
