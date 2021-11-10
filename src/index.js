b879689bef3bf8b6a09d2190253dbda29ecd0b7e
morghull <morghull.study@gmail.com>
2021-11-10 16:04:50 +0200

    add cache to pow function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './style.css';
 import Component from './Component';
-import secret, * as MyMath from './math';
 import { pow } from './math';
 
-const resultOfPow = MyMath.pow(5, 3);
-const resultOfPow1 = pow(5, 3);
+const resultOfPow = pow(456, 88);
 console.log(resultOfPow);
+
 const comp = new Component(12);
 console.log(comp.render());
-console.log(secret);
+
--- src/math.js
+++ src/math.js
@@ -1,13 +1,14 @@
+const cache = new Map();
+
 export const pow = (base, exp) => {
-  return base ** exp;
+  const key = `${base}_${exp}`;
+  if (cache.has(key)) return cache.get(key);
+  const value = base ** exp;
+  cache.set(key, value);
+  return value;
 };
 
 export const sum = (...args) =>
   args.reduce((res, args) => res + args, 0);
 
 export const mult = (a, b) => a * b;
-
-const superSecret = 'Elon Musk';
-
-const ssFunction = () => superSecret;
-export default superSecret;
